ecb461b3d7e1dd70fea7191dca54006068cd4a03
Raghav Mehra <ragmehra@hotmail.com>
2021-12-23 09:09:17 -0800

    Setup colorGrid and clearGrid functions

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-
-
-
-function colorGrid (e) {
-    e.target.style.backgroundColor = "black";
-
-}
-
-
-function createGrid () { 
+function createGrid (totalRows, totalColumns) { 
+    totalRows = 16;
+    totalColumns = 16;
     const main = document.querySelector("main");
     const gridContainer = document.createElement("div");
-    gridContainer.classList.add("container");
+    gridContainer.classList.add("gridContainer");
     main.appendChild(gridContainer);
 
-    for (let numOfRows = 0; numOfRows < 16; numOfRows++) {
+    for (let numOfRows = 0; numOfRows < totalRows; numOfRows++) {
         const newRow = document.createElement("div");
         newRow.classList.add("row");
-        newRow.textContent = "-->";
         gridContainer.appendChild(newRow);
 
-        for (let numOfColumns = 0; numOfColumns < 16; numOfColumns++) {
+        for (let numOfColumns = 0; numOfColumns < totalColumns; numOfColumns++) {
             const newColumn = document.createElement("div");
-            newColumn.textContent = ".";
             newColumn.classList.add("column");
             newRow.appendChild(newColumn);
         }
     }
+
+    const reset = document.createElement("button");
+    reset.classList.add("reset");
+    reset.textContent = "Reset";
+    main.appendChild(reset);
+}
+
+function colorGrid (event) {
+    event.target.style.backgroundColor = "black";
+
+}
+
+function clearGrid (event) {
+    const columns = document.querySelectorAll(".column");
+    for (let column of columns) {
+        console.log(column);
+        column.style.backgroundColor = "white";
+    }
+
 }
 
 createGrid();
+function hover (event) {
+    event.addEventListener("mouseover", colorGrid);
+}
+const columns = document.querySelectorAll(".column");
+columns.forEach(hover);
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", clearGrid);
+
